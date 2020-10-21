d52a037592d527d634f977200d138ebe7937cb15
Daphne Reed <daphne.reed@vidyard.com>
2020-10-21 15:17:34 -0400

    OSINT calendar checker

--- OSINT-Calendars.sh
+++ OSINT-Calendars.sh
@@ -1,5 +1,14 @@
 #/bin/bash
 
 
+while IFS= read -r user; do
+  page_content=$(curl -s "https://calendar.google.com/calendar/u/0/embed?src=$user")
+  
+  
+  if [[ $page_content == *"Moved Temporarily"* ]]; then
+    echo -e "\e[32m$user calendar is not shared"
+  else echo -e "\e[31m$user calendar is PUBLIC"
+  fi
 
-https://calendar.google.com/calendar/u/0/embed?src=
+
+done < UserList.txt
